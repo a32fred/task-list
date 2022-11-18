e52aee986d75b0ed4b48a444c7930b25ec4b902c
a32fred <a.32.freeed@gmail.com>
2022-11-18 14:30:32 -0300

    interface vizual finalizada

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,18 @@ function App() {
 
         <h3>Lista de Tarefas</h3>
 
-        <div className="task-area">
-          <label for="task" className="task">Task 1</label>
-          <input type="checkbox" id="task"/>
+        <div className="addCampo">
+          <input type="text" name="" id="" />
+          <button>add+</button>
         </div>
-       
-        <button>Add+</button>
+      <hr size="1" width="80%"/>
+        <div className="container-task-area">
+          <div className="task-area">
+            <label for="task" className="task">Task 1:</label>
+            <input type="checkbox" id="task"/>
+          </div>
+        </div>
+        
       </div>
     </div>
   )
